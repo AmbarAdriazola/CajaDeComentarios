7fdf40d4e7988da87f58d895573e3112f19d60a6
Ámbar <ambar.a.q35@gmail.com>
2023-05-04 16:30:59 -0400

    prueba1

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
       <div className='contenedor-principal'>
        <h1>Esto es lo que dicen los alumnos sobre freeCodeCamp:</h1>
        <Testimonio 
-          nombre='Andrea Peres'
+          nombre='Andrea Perez'
           pais='Holanda'
           imagen='andrea'
           cargo='Ingeniera Software'
